de8fb458aa07865a0c5a4626f865aa709dc6a335
cameron joseph <cameronjoseph357@gmail.com>
2026-04-08 22:03:51 -0700

    Fix reexport with db_coercion to pass ruff audit

--- trading/database/db_coercion.py
+++ trading/database/db_coercion.py
@@ -20,6 +20,24 @@ from common.coercion import (
     row_str,
 )
 
+__all__ = [
+    "coerce_bool",
+    "coerce_float",
+    "coerce_int",
+    "coerce_str",
+    "expect_float",
+    "expect_int",
+    "expect_str",
+    "row_expect_float",
+    "row_expect_int",
+    "row_expect_str",
+    "row_float",
+    "row_int",
+    "row_str",
+    "to_float_obj",
+    "to_int_obj",
+]
+
 
 def to_float_obj(value: object) -> object:
     return expect_float(value)
